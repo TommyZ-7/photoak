f96391f9af7d2877ffb224ddadcacdc14877deda
TK7 <tktomo7260t@gmail,com>
2024-11-26 13:49:15 +0900

    EditPageコンポーネントの画像表示スタイルから不要なcropスタイルを削除

--- src/app/tui/page.tsx
+++ src/app/tui/page.tsx
@@ -3,7 +3,6 @@ export default function Home() {
   return (
     <main className="container mx-auto p-4">
       <img src="002.jpg" alt="Vercel Logo" 
-      style={{ crop: "inset(40% 0% 0% 0%)"}}
       />
     </main>
   );
